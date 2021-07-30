26b866c8b3026094c3e04d58304304d04b7a3994
Mason Neu <72635358+xSaoul@users.noreply.github.com>
2021-07-30 10:04:25 -0500

    Fix colour.js and add booster funtionality
    colour.js is now a booster only command, also removed all commented out and duplicated code.

--- commands/utility/colour.js
+++ commands/utility/colour.js
@@ -8,22 +8,14 @@ module.exports = {
     cooldown: 0,
     description: "allows the user to set a new colour for their embeds",
     async execute(client, message, args, Discord, economy, util){
-
+        
+        if(!message.member.roles.cache.has("868656454054342677")) return;
+        
         var re = /[0-9A-Fa-f]{6}/g
         if(re.test(args[1])) {
             var filter = m => m.author.id == message.author.id;
-                        
-                // try {
-                //     var colourEmbed = Discord.MessageEmbed()
-                //         .setTitle("Are you sure this is the colour you want")
-                //         .setDescription("**Message Y / N**")
-                //         .setColor(args[1])
-                //     message.channel.send(colourEmbed)
-                //     var collectedMessages = await message.channel.awaitMessages(filter, {time: 5000, max: 1, errors: ['time']});
-                // } catch (e) {return message.reply("Looks like you took too long to reply!");};
 
                 try {
-                    // message.reply(`Are you sure you want to buy the \`reinforced pickaxe\`?\n\n**message Y or N**`)
                     var colourEmbed = new Discord.MessageEmbed()
                         .setTitle("Are you sure this is the colour you want")
                         .setDescription("**Message Y / N**")
