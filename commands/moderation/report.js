f06d7128012384e0d4527c68d2dacbe903049860
krazyunderground <charlieprescod@outlook.com>
2021-08-10 23:28:01 +0100

    %edit logs, #delete logs

--- commands/moderation/report.js
+++ commands/moderation/report.js
@@ -1,57 +1,44 @@
-const Discord = require("discord.js");
+const Discord = require("discord.js")
 
 module.exports = {
-  name: "report",
-  aliases: ["rep"],
-  category: "admin",
-  use: "!m report",
-  description: "report a message so mods can investigate.",
-  cooldown: 0,
-  async execute(client, message, args, Discord, economy, util) {
-    const gp = await client.functions.get("checkGuild").execute(message);
-    if (!args[1])
-      return message.reply(
-        "You need to provide the ID of the message!\n\nIf you don't know how to get a message's ID, refer to this tutorial!\n<https://www.howtogeek.com/714348/how-to-enable-or-disable-developer-mode-on-discord/>"
-      );
-    const reportID = args[1];
-    const reportChannel = message.channel.id;
-    if (!gp.reportChannel)
-      return message.channel.send(
-        "Reports aren't set up for this server! Please refer to server staff for more info!"
-      );
-    const sendChannel = client.channels.cache.get(gp.reportChannel);
+    name: "report",
+    aliases: ['rep'],
+    category: "admin",
+    use: "!m report",
+    description: "report a message so mods can investigate.",
+    cooldown: 0,
+    async execute(client, message, args, Discord, economy, util){
+        const gp = await client.functions.get("checkGuild").execute(message)
+        if(!args[1]) return message.reply("You need to provide the ID of the message!\n\nIf you don't know how to get a message's ID, refer to this tutorial!\n<https://www.howtogeek.com/714348/how-to-enable-or-disable-developer-mode-on-discord/>")
+        const reportID = (args[1])
+        const reportChannel = message.channel.id
+        if(!gp.reportChannel) return message.channel.send("Reports aren't set up for this server! Please refer to server staff for more info!")
+        const sendChannel = client.channels.cache.get(gp.reportChannel)
+        
+        try{ 
+            
+            message.guild.channels.cache.get(message.channel.id).messages.fetch(reportID).then(report => {
+                let repAuthor = report.author
+                let authorID = report.author.id
+                let fetchContent = report.content
+                const reportEmbed = new Discord.MessageEmbed()
+                .setTitle("New Report")
+                .setColor("#FF9CA9")
+                .setDescription(`${message.author} has reported ${repAuthor} (${authorID})`)
+                .addFields(
+                    { name: "**Channel**", value: `<#${reportChannel}>`},
+                    { name: "**Message ID**", value: `[${reportID}](${report.url})`},
+                    { name: "**Message**", value: `${fetchContent}`}
+                )
+                sendChannel.send({embeds: [reportEmbed]})
+                message.channel.send("Report sent, thank you!")
 
-    try {
-      message.guild.channels.cache
-        .get(message.channel.id)
-        .messages.fetch(reportID)
-        .then((report) => {
-          let repAuthor = report.author;
-          let authorID = report.author.id;
-          let fetchContent = report.content;
-          const reportEmbed = new Discord.MessageEmbed()
-            .setTitle("New Report")
-            .setColor("#FF9CA9")
-            .setDescription(
-              `${message.author} has reported ${repAuthor} (${authorID})`
-            )
-            .addFields(
-              { name: "**Channel**", value: `<#${reportChannel}>` },
-              { name: "**Message ID**", value: `[${reportID}](${report.url})` },
-              { name: "**Message**", value: `${fetchContent}` }
-            );
-          sendChannel.send({ embeds: [reportEmbed] });
-          message.channel.send("Report sent, thank you!");
-        })
-        .catch((err) =>
-          message.channel.send(
-            "Looks like something went wrong!\nPlease verify the ID is correct, and try again!"
-          )
-        );
-    } catch (err) {
-      message.channel.send(
-        "Looks like something went wrong!\nPlease try again!"
-      );
-    }
-  },
+            }).catch((err) => message.channel.send("Looks like something went wrong!\nPlease verify the ID is correct, and try again!"))
+
+
+        } catch (err) {
+            message.channel.send("Looks like something went wrong!\nPlease try again!")
+            
+        }
+    },
 };
--- events/guild/messageDelete.js
+++ events/guild/messageDelete.js
@@ -1,4 +1,5 @@
 module.exports = async (Discord, client, message) => {
+    if (message.embeds.size > 0) return;
     const gp = await client.functions.get("checkGuild").execute(message)
     if(!gp.logsChannel) return;
     const logsChannel = client.channels.cache.get(gp.logsChannel)
@@ -6,7 +7,36 @@ module.exports = async (Discord, client, message) => {
     const deleteChannel = message.channel.id
 
     const fetchContent = message.content
-
+    let contExists = true;
+    if(!fetchContent) {contExists = false}
+    console.log(fetchContent)
+    //attachment and content
+    if (message.attachments.size > 0 && contExists == true) {
+        const deleteEmbed = new Discord.MessageEmbed()
+        .setAuthor(message.member.user.tag, message.author.displayAvatarURL({dynamic: true}))
+        .setTitle("Message Deleted")
+        .setColor("#FF1C48")
+        .setImage(message.attachments.first().proxyURL)
+        .addFields(
+            { name: "**Channel**", value: `<#${deleteChannel}>`},
+            { name: "**Message**", value: `${fetchContent}`},
+        )
+        logsChannel.send({ embeds: [deleteEmbed] });
+      }
+      //attachment only
+      else if (message.attachments.size > 0 && contExists == false){ 
+        const deleteEmbed = new Discord.MessageEmbed()
+        .setAuthor(message.member.user.tag, message.author.displayAvatarURL({dynamic: true}))
+        .setTitle("Message Deleted")
+        .setColor("#FF1C48")
+        .setImage(message.attachments.first().proxyURL)
+        .addFields(
+            { name: "**Channel**", value: `<#${deleteChannel}>`},
+        )
+        logsChannel.send({ embeds: [deleteEmbed] });
+      }
+      //content only
+       else {
     const deleteEmbed = new Discord.MessageEmbed()
     .setAuthor(message.member.user.tag, message.author.displayAvatarURL({dynamic: true}))
     .setTitle("Message Deleted")
@@ -16,4 +46,5 @@ module.exports = async (Discord, client, message) => {
         { name: "**Message**", value: `${fetchContent}`}
     )
     logsChannel.send({ embeds: [deleteEmbed] });
+      }
 }
