2a36c342e6eb8a0bc31e97c21ee67c621bfc6d6b
xSaoul <masonneu22@gmail.com>
2021-09-17 20:33:28 -0500

    +coinflip +roll

---
+++ commands/fun/coinflip.js
@@ -0,0 +1,48 @@
+const userEcon = require('../../models/userEcon')
+module.exports = {
+    name: "coinflip",
+    aliases: ["cf"],
+    category: "basic",
+    use: "!m coinflip <amount>",
+    cooldown: 1,
+    description: "flip a coin.",
+    async execute(client, message, args, Discord, economy, util){
+        let amount = args[1];
+        if (isNaN(args[1]) || amount % 1 != 0) return message.channel.send("Please make sure the amount is a whole number!")
+        const memberBal = await client.functions.get("getAuthorEcon").execute(message);
+        if (memberBal.coins < amount) return message.channel.send("You don't have enough coins!")
+        const randomNumber = Math.floor(Math.random() * 2) + 1;
+        let result = 0;
+        if(randomNumber >= 2) {
+            result = amount * .5
+            await userEcon.findOneAndUpdate(
+                {
+                    userID: message.author.id
+                },
+                {
+                    $inc:{
+                        coins: result
+                    }
+                }
+            )
+            message.channel.send(`You won \`${result}\` coins!`)
+            return
+        }
+        if(randomNumber <= 1) {
+            result = amount
+            await userEcon.findOneAndUpdate(
+                {
+                    userID: message.author.id
+                },
+                {
+                    $inc:{
+                        coins: -result
+                    }
+                }
+            )
+            message.channel.send(`You lost \`${result}\` coins!`)
+            return
+        }
+        
+    }
+}
---
+++ commands/fun/roll.js
@@ -0,0 +1,47 @@
+const userEcon = require('../../models/userEcon')
+module.exports = {
+    name: "roll",
+    category: "basic",
+    use: "!m roll <amount>",
+    cooldown: 1,
+    description: "Try to roll a higher number than me.",
+    async execute(client, message, args, Discord, economy, util){
+        let amount = args[1];
+        if (isNaN(args[1]) || amount % 1 != 0) return message.channel.send("Please make sure the amount is a whole number!")
+        const memberBal = await client.functions.get("getAuthorEcon").execute(message);
+        if (memberBal.coins < amount) return message.channel.send("You don't have enough coins!")
+        let userRoll = Math.floor(Math.random() * 6) + 1;
+        let botRoll = Math.floor(Math.random() * 6) + 1;
+        let result = 0;
+        if(userRoll > botRoll) {
+            result = amount * .5
+            await userEcon.findOneAndUpdate(
+                {
+                    userID: message.author.id
+                },
+                {
+                    $inc:{
+                        coins: result
+                    }
+                }
+            )
+            message.channel.send(`You won \`${result}\` coins with a \`${userRoll}\`!`)
+            return
+        }
+        if(botRoll > userRoll) {
+            result = amount
+            await userEcon.findOneAndUpdate(
+                {
+                    userID: message.author.id
+                },
+                {
+                    $inc:{
+                        coins: -result
+                    }
+                }
+            )
+            message.channel.send(`You lost \`${result}\` coins with a \`${userRoll}\`!`)
+            return
+        }
+    }
+}
