30be961a9346de84945c7b96b6183578ca2bb4a0
xSaoul <72635358+xSaoul@users.noreply.github.com>
2021-12-07 15:37:00 -0600

    %inventory

--- commands/economy/inventory.js
+++ commands/economy/inventory.js
@@ -6,28 +6,62 @@ module.exports = {
     use: "inventory",
     cooldown: 5,
     maxArgs: 0,
-    description: "allows the user to check their item inventory.",
+    description: "displays the command users inventory.",
     async execute(client, message, args, Discord, economy, util) {
         const userecon = await client.functions.get("getAuthorEcon").execute(message)
         const userutil = await client.functions.get("getUtil").execute(message)
         owns = userecon.owns.split(" ")
         ownsf = []
         owns.forEach(item => {
-            if(item.endsWith("h")){
-                ownsf.push(`\`${item.slice(0, -1)} helmet\``)
+            const tier = item.slice(0, 1)
+            const id = item.slice(1, 2)
+            let alloy;
+            let piece;
+            switch(tier) {
+                case "1":
+                    alloy = "Steel"
+                break
+                case "2":
+                    alloy = "Magnite"
+                break
+                case "3":
+                    alloy = "Elgiloy"
+                break
+                case "4":
+                    alloy = "Shakudo"
+                break
+                case "5":
+                    alloy = "Stellite"
+                break
+                case "6":
+                    alloy = "Cobium"
+                break
+                case "7":
+                    alloy = "Dymalloy"
+                break
+                case "8":
+                    alloy = "Vitallium"
+                break
             }
-            if(item.endsWith("c")){
-                ownsf.push(`\`${item.slice(0, -1)} chestplate\``)
+            switch(id) {
+                case "h":
+                    piece = "Helmet"
+                break
+                case "c":
+                    piece = "Chestplate"
+                break
+                case "l":
+                    piece = "Leggings"
+                break
+                case "b":
+                    piece = "Boots"
+                break
+                case "s":
+                    piece = "Sword"
+                break
             }
-            if(item.endsWith("l")){
-                ownsf.push(`\`${item.slice(0, -1)} leggings\``)
-            }
-            if(item.endsWith("b")){
-                ownsf.push(`\`${item.slice(0, -1)} boots\``)
-            }
-            if(item.endsWith("s")){
-                ownsf.push(`\`${item.slice(0, -1)} sword\``)
-            }
+            const fItem = alloy + " " + piece
+            ownsf.push(fItem)
         })
         const embed = new Discord.MessageEmbed()
             .setAuthor(message.member.user.tag, message.member.user.displayAvatarURL())
